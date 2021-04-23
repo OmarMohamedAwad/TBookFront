7d361c3e38c1db4d85513156eebfab3661840a6b
Omar.M.Awad <omar.awad825@gmail.com>
2021-04-23 16:48:21 +0200

    profile 2

--- src/app/modules/user layout/user-profile/services/user-profile.service.ts
+++ src/app/modules/user layout/user-profile/services/user-profile.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 })
 export class UserProfileService {
 
-  readonly baseURL: string = "http://localhost:3000/users";
-  readonly rateURL: string = "http://localhost:3000/rating";
+  readonly baseURL: string = "https://tbookback.herokuapp.com/users";
+  readonly rateURL: string = "https://tbookback.herokuapp.com/rating";
 
   constructor(private userProfileClient: HttpClient) { }
 
